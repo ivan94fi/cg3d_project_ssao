91d80e6c29b4cd55c50daf3c7a252d8db7517d4a
Ivan Prosperi <ivan94fi@gmail.com>
2020-02-11 15:21:00 +0100

    Add and configure copy-webpack-plugin

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './js/main.js',
@@ -21,12 +22,14 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: './index.html',
-            favicon: './resources/icon.png',
             inject: true,
             minify: {
                 removeComments: true,
                 collapseWhitespace: false
             }
-        })
+        }),
+        new CopyWebpackPlugin([
+            { from: './resources', to: './resources' }
+        ]),
     ],
 };
